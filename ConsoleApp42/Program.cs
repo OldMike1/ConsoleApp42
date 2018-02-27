1677fb932053692f811bf42ddd65020250fdd9e7
mike19952006 <mike19952006@gmail.com>
2018-02-27 02:52:30 +0200

    Рисовалка.  Создать границы.

--- ConsoleApp42/Program.cs
+++ ConsoleApp42/Program.cs
@@ -10,6 +10,42 @@ namespace ConsoleApp42
     {
         static void Main(string[] args)
         {
+            int x = Console.WindowWidth / 2;
+            int y = Console.WindowHeight / 2;
+            Console.SetCursorPosition(x, y);
+            int p = Console.WindowWidth-1;
+            
+
+            while (true)
+            {
+                ConsoleKeyInfo keyinfo = Console.ReadKey();
+                if (keyinfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (y == 0)
+                        Console.SetCursorPosition(x, y);
+                    else
+                        Console.SetCursorPosition(x, y -= 1);
+                }
+                if (keyinfo.Key == ConsoleKey.DownArrow)
+                {
+                        Console.SetCursorPosition(x, y += 1);
+                }
+                if (keyinfo.Key == ConsoleKey.RightArrow)
+                {
+                    if (x == p)
+                        Console.SetCursorPosition(x, y);
+                    else
+                        Console.SetCursorPosition(x += 1, y);
+                }
+                if (keyinfo.Key == ConsoleKey.LeftArrow)
+                {
+                    if (x == 0)
+                        Console.SetCursorPosition(x, y);
+                    else
+                        Console.SetCursorPosition(x -= 1, y);
+                }
+                Console.WriteLine("*");
+            }
         }
     }
 }
